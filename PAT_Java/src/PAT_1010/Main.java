491097a898f253ad3fa8dce06fb216786b996cb4
Tang <841449529@qq.com>
2019-09-07 21:00:12 +0800

    提交题解PAT1010

---
+++ PAT_Java/src/PAT_1010/Main.java
@@ -0,0 +1,71 @@
+package PAT_1010;
+
+
+
+/***
+ * @description
+ * @author Tang
+ * @Date 2019/9/7
+ */
+
+
+import java.util.Scanner;
+
+/**
+ * 设计函数求一元多项式的导数。（注：x^n（n为整数）的一阶导数为nx^n−1
+ * ​​ 。）
+ *
+ * 输入格式:
+ * 以指数递降方式输入多项式非零项系数和指数（绝对值均为不超过 1000 的整数）。数字间以空格分隔。
+ *
+ * 输出格式:
+ * 以与输入相同的格式输出导数多项式非零项的系数和指数。数字间以空格分隔，但结尾不能有多余空格。注意“零多项式”的指数和系数都是 0，但是表示为 0 0。
+ */
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean flag =false;
+        while (sc.hasNext()){
+            int M = sc.nextInt(); // 系数
+            int N = sc.nextInt(); // 指数
+            if (M*N!=0){
+                if (flag){
+                    System.out.print(" ");
+                }else {
+                    flag = true;
+                }
+                System.out.print(M*N+" "+(N-1));
+            }
+        }
+        sc.close();
+        if (!flag){
+            System.out.print("0 0");
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
